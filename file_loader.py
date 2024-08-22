6ebf0d1309ba8866071318c53860cc7d0ced2d8a
valosekj <jan.valosek@upol.cz>
2024-08-22 14:00:51 -0400

    Refactor the script to allow selecting the images for further processing from the list of images

--- file_loader.py
+++ file_loader.py
@@ -13,6 +13,7 @@ Author: Jan Valosek and Claude 3.5 Sonnet
 import os
 import shutil
 import argparse
+import pandas as pd
 import nibabel as nib
 
 
@@ -51,80 +52,39 @@ def get_parser():
     return parser.parse_args()
 
 
-def get_valid_file_path(prompt):
+def get_image_info(file_path):
     """
-    Repeatedly prompt the user for a file path until a valid file is specified or the user aborts.
-
-    Args:
-    prompt (str): The input prompt to display to the user.
-
-    Returns:
-    str: The valid file path.
-    """
-    while True:
-        file_path = input(prompt)
-        # Get absolute path, expand user path
-        file_path = os.path.abspath(os.path.expanduser(file_path))
-        if os.path.isfile(file_path):
-            return file_path
-        print(f"Error: File does not exist: {file_path}"
-              f"\nPlease try again.")
-
-
-def print_image_info(file_path):
-    """
-    Print the dimensions and pixel size of the image at the given file path.
+    Get the dimensions and pixel size of the image at the given file path.
 
     Args:
     file_path (str): Path to the image file.
     """
     img = nib.load(file_path)
     zooms = img.header.get_zooms()
-    print(f"Dimensions: {img.shape[0]} x {img.shape[1]} x {img.shape[2]}")
-    print(f"Pixel size: {zooms[0]:.2f} mm x {zooms[1]:.2f} mm x {zooms[2]:.2f} mm")
+
+    dimensions = f"{img.shape[0]} x {img.shape[1]} x {img.shape[2]}"
+    pixel_size = f"{zooms[0]:.2f} mm x {zooms[1]:.2f} mm x {zooms[2]:.2f} mm"
+
+    return dimensions, pixel_size
 
 
-def confirm_image_info(file_path):
-    """
-    Display the image dimensions and pixel size and ask the user to confirm if it's correct.
-
-    Args:
-    file_path (str): Path to the image file.
-
-    Returns:
-    bool: True if the user confirms the information, False otherwise.
-    """
-    while True:
-        print_image_info(file_path)
-        confirm = input("Do you agree with this image info? (y/n): ").lower()
-        if confirm in ['y', 'yes', 'Y', 'YES']:
-            return True
-        elif confirm in ['n', 'no', 'N', 'NO']:
-            return False
-        else:
-            print("Invalid input. Please enter 'y' or 'n'.")
-
-
-def run_dcm2niix(dicom_folder, output_folder):
+def run_dcm2niix(dicom_folder, temp_folder):
     """
     Run dcm2niix command to convert DICOM images to NIfTI format.
 
     Args:
     dicom_folder (str): Path to the folder containing DICOM images.
-    output_folder (str): Path to the output folder for NIfTI images.
+    temp_folder (str): Path to the temporary folder for NIfTI images.
 
     Returns:
     tuple: Paths to the converted T2w and DWI NIfTI files.
     """
-    # Create a temporary output folder to store dcm2niix output before renaming the files
-    temp_folder = os.path.join(output_folder, "temp_dcm2niix")
-    os.makedirs(temp_folder, exist_ok=True)
 
     cmd = [
         "dcm2niix",
-        "-z", "y",          # Compress output
-        "-f", "%f_%p_%s",   # Custom filename format: %f - folder name, %p - protocol name, %s - series number
-        "-i", "y",          # Ignore derived, localizer and 2D images
+        "-z", "y",      # Compress output
+        "-f", "%p_%s",  # Custom filename format: %p - protocol name, %s - series number
+        "-i", "y",      # Ignore derived, localizer and 2D images
         "-o", temp_folder,
         dicom_folder
     ]
@@ -134,88 +94,107 @@ def run_dcm2niix(dicom_folder, output_folder):
     os.system(" ".join(cmd))
 
 
-def get_t2_image_path():
+def select_image(contrast, nii_info_df, temp_folder):
     """
-    Get the path to the T2w image and check the image information.
+    Select an image from the list of images and return the selected image path.
 
-    Returns:
-    str: Valid T2w image path.
+    :param contrast: Contrast type, e.g., T2w, dwi
+    :param nii_info_df: DataFrame with image information
+    :param temp_folder: Path to the temporary folder with NIfTI images
+    :return: Path to the selected image
     """
-    print(10*"-")
-    print("T2w image")
-    print(10*"-")
-    # Get T2w image path
+    # Ask the user to provide a row number (df index) corresponding to the image
     while True:
-        path_t2w = get_valid_file_path("Please specify the path to the T2w image: ")
-
-        # If the image info is confirmed, return the image path
-        if confirm_image_info(path_t2w):
-            return path_t2w
-        print("Let's try another T2w image.")
-
-
-def get_dwi_image_path():
-    """
-    Get the path to the DWI image and check for the existence of bval and bvec files.
-
-    Returns:
-    str: Valid DWI image path.
-    """
-    print(10*"-")
-    print("DWI image")
-    print(10*"-")
-    # Get DWI image path
-    while True:
-        # Ask the user for the DWI image path
-        path_dwi = get_valid_file_path("Please specify the path to the DWI image: ")
-
-        # Check for bval and bvec files
-        dwi_base = path_dwi.replace('.nii', '').replace('.gz', '')
-        bval_path = f"{dwi_base}.bval"
-        bvec_path = f"{dwi_base}.bvec"
-
-        # Check whether both bval and bvec files exist (we need them for DWI processing to compute DTI model)
-        if not os.path.isfile(bval_path) or not os.path.isfile(bvec_path):
-            print("Error: bval or bvec file is missing for the provided DWI image."
-                  "\nPlease try another DWI image.")
+        row_number = int(input(f"Please specify the row number of the {contrast} image you want to use: "))
+        if row_number < 0 or row_number >= len(nii_info_df):
+            print("Error: Invalid image number. Please try again.")
             continue
         else:
-            print("bval and bvec files found.")
+            fname = nii_info_df.iloc[row_number]['File Name']
+            if contrast == "dwi":
+                if not validate_dwi_image(os.path.join(temp_folder, fname)):
+                    continue
 
-        # If the image info is confirmed, return the image path
-        if confirm_image_info(path_dwi):
-            return path_dwi
-        print("Let's try another DWI image.")
+            print(f"Selected {contrast} image: {fname}")
+            return os.path.join(temp_folder, fname)
 
 
-def copy_files_to_bids_folder(output_folder, participant_id, session_id, path_t2w, path_dwi):
+def validate_dwi_image(fname):
     """
-    Copy the converted nii images from the temporary folder to the output BIDS folder
+    Check the existence of bval and bvec files for the DWI image.
+
+    :param fname: DWI image file name
+    """
+
+    # Check for bval and bvec files
+    dwi_base = fname.replace('.nii', '').replace('.gz', '')
+    fname_bval = f"{dwi_base}.bval"
+    fname_bvec = f"{dwi_base}.bvec"
+
+    # Check whether both bval and bvec files exist (we need them for DWI processing)
+    if not os.path.isfile(fname_bval) or not os.path.isfile(fname_bvec):
+        print("Error: bval or bvec file is missing for the provided DWI image."
+              "\nPlease try another DWI image.")
+        return False
+    else:
+        return True
+
+
+def get_nii_info_dataframe(temp_folder):
+    # Get all nii files in the temporary folder
+    nii_files = [f for f in os.listdir(temp_folder) if f.endswith('.nii.gz')]
+
+    # Create lists to store the information
+    file_names = []
+    dimensions_list = []
+    pixel_sizes = []
+
+    # Collect information for each file
+    for nii_file in nii_files:
+        nii_path = os.path.join(temp_folder, nii_file)
+        dimensions, pixel_size = get_image_info(nii_path)
+
+        file_names.append(nii_file)
+        dimensions_list.append(dimensions)
+        pixel_sizes.append(pixel_size)
+
+    # Create a DataFrame
+    df = pd.DataFrame({
+        'File Name': file_names,
+        'Dimensions': dimensions_list,
+        'Pixel Size': pixel_sizes
+    })
+
+    return df
+
+
+def copy_files_to_bids_folder(contrast, fname, output_folder, participant_id, session_id):
+    """
+    Copy the converted nii image and its accompanying JSON sidecar from the temporary folder to the output BIDS folder.
+    For DWI images, also copy the bval and bvec files.
+
+    :param contrast: Contrast type, e.g., T2w, dwi
+    :param fname: Path to the converted nii image in the temporary folder
     :param output_folder: temporary folder with the converted nii images
     :param participant_id: participant ID, e.g., sub-001
     :param session_id: session ID, e.g., ses-01
-    :param path_t2w: path to the T2w image provided by the user
-    :param path_dwi: path to the DWI image provided by the user
     """
     # First, create anat and dwi subfolders if they do not exist
-    output_folder_anat = os.path.join(output_folder, "anat")
-    os.makedirs(output_folder_anat, exist_ok=True)
-    output_folder_dwi = os.path.join(output_folder, "dwi")
-    os.makedirs(output_folder_dwi, exist_ok=True)
+    if contrast == "dwi":
+        contrast_folder = "dwi"
+    else:
+        contrast_folder = "anat"
+    output_folder = os.path.join(output_folder, contrast_folder)
+    os.makedirs(output_folder, exist_ok=True)
 
     # Second, move the images and JSON sidecars to the respective folders
-    path_t2w_output = os.path.join(output_folder_anat, f"{participant_id}_{session_id}_T2w.nii.gz")
-    path_dwi_output = os.path.join(output_folder_dwi, f"{participant_id}_{session_id}_dwi.nii.gz")
-    shutil.copy(path_t2w, path_t2w_output)
-    shutil.copy(path_t2w.replace('.nii.gz', '.json'), path_t2w_output.replace('.nii.gz', '.json'))
+    fname_output = os.path.join(output_folder, f"{participant_id}_{session_id}_{contrast}.nii.gz")
+    shutil.copy(fname, fname_output)
+    shutil.copy(fname.replace('.nii.gz', '.json'), fname_output.replace('.nii.gz', '.json'))
     # For DWI, we also need to copy the bval and bvec files
-    shutil.copy(path_dwi, path_dwi_output)
-    shutil.copy(path_dwi.replace('.nii.gz', '.json'), path_dwi_output.replace('.nii.gz', '.json'))
-    shutil.copy(path_dwi.replace('.nii.gz', '.bval'), path_dwi_output.replace('.nii.gz', '.bval'))
-    shutil.copy(path_dwi.replace('.nii.gz', '.bvec'), path_dwi_output.replace('.nii.gz', '.bvec'))
-
-    # Lastly, remove the temporary folder
-    shutil.rmtree(os.path.join(output_folder, "temp_dcm2niix"))
+    if contrast == "dwi":
+        shutil.copy(fname.replace('.nii.gz', '.bval'), fname_output.replace('.nii.gz', '.bval'))
+        shutil.copy(fname.replace('.nii.gz', '.bvec'), fname_output.replace('.nii.gz', '.bvec'))
 
 
 def main():
@@ -252,21 +231,40 @@ def main():
         os.makedirs(output_folder, exist_ok=True)
         print(f"\nInfo: Converted NIfTI images will be stored in: {output_folder}")
 
+    # Create a temporary folder to store dcm2niix output before renaming the files
+    temp_folder = os.path.join(output_folder, "temp_dcm2niix")
+    print(f"\nInfo: Creating a temporary folder for NIfTI images: {temp_folder}")
+    os.makedirs(temp_folder, exist_ok=True)
     # Run DICOM to NIfTI conversion using the dcm2niix command
-    run_dcm2niix(dicom_folder, output_folder)
+    run_dcm2niix(dicom_folder, temp_folder)
 
     print(100*"-")
-    print("DICOM to NIfTI is done. Please review the images and provide paths to the T2w and DWI images.")
+    print("DICOM to NIfTI is done. Please review the images and select images for further processing.")
     print(100*"-")
 
-    # Get the paths to the converted T2w and DWI images
-    path_t2w = get_t2_image_path()
-    path_dwi = get_dwi_image_path()
+    nii_info_df = get_nii_info_dataframe(temp_folder)
+    # Display the DataFrame
+    print(f'{nii_info_df}\n')
+
+    # Select images intended for further processing
+    images_to_use_dict = {}
+    for contrast in ["T2w", "dwi"]:
+        images_to_use_dict[contrast] = select_image(contrast, nii_info_df, temp_folder)
 
     # Copy the files to the BIDS folder
-    copy_files_to_bids_folder(output_folder, participant_id, session_id, path_t2w, path_dwi)
+    for contrast, fname in images_to_use_dict.items():
+        print(f"Copying {contrast} image to the BIDS folder.")
+        copy_files_to_bids_folder(contrast, fname, output_folder, participant_id, session_id)
 
-    print("\nAll files have been successfully validated and confirmed.")
+    # Lastly, remove the temporary folder
+    print("\nInfo: Removing the temporary folder {temp_folder}")
+    shutil.rmtree(os.path.join(output_folder, "temp_dcm2niix"))
+
+    print(100*"-")
+    print("All files have been successfully converted and validated. You can find the following images in the "
+          "BIDS folder:")
+    print(f"\t{output_folder}")
+    print(100*"-")
 
 
 if __name__ == "__main__":
